81d68858e1e4d7e92273dcc72bf68dead0ec905e
fengli <fengli@2dfire.com>
2017-03-21 16:34:27 +0800

    add header file

---
+++ DHSplitCells/Classes/DHSplitCells.h
@@ -0,0 +1,16 @@
+//
+//  DHSplitCells.h
+//  Pods
+//
+//  Created by 凤梨 on 17/3/21.
+//
+//
+
+#ifndef DHSplitCells_h
+#define DHSplitCells_h
+
+#import "DHSplitCellProtocol.h"
+#import "DHSplitCellBaseModel.h"
+#import "DHSplitCellDispatch.h"
+
+#endif /* DHSplitCells_h */
